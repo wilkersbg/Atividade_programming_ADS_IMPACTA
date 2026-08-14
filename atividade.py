7be61fb2dd643d53c3737fd037b563096e32834b
Wilker Sammy <wilkerww.n@gmail.com>
2026-08-13 22:31:12 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,22 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+
+soma = a + b #somar 
+subtracao = a - b #subtrair
+multiplicacao = a * b #multiplicação
+divisao_intera = a // b #divisao inteira
+resto = a % b # resto da divisão
+potencia = a ** b #potencia 
+divisao = a / b #divisao comum
+
+print(f' Soma: {a} + {b} é igual a {soma}')
+print(f' Subtração: {a} - {b} é igual a {subtracao}')
+print(f' multiplicação: {a} * {b} é igual a {multiplicacao}')
+print(f' divisão inteira: {a} // {b} é igual a {divisao_intera}')
+print(f' resto da divisão: {a} % {b} é igual a {resto}')
+print(f' potência: {a} ** {b} é igual a {potencia}')
+print(f' divisão comum: {a} / {b} é igual a {divisao}')
+
+x = 12
+resultado = x + (2 * x)
+print (f'O resultado da expressão x + (2 * x) é igual a {resultado}')
