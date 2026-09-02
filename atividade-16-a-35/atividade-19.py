fdac5cd8084b79c71375e171e6bb9d63f00fda47
Wilker Sammy <wilkerww.n@gmail.com>
2026-09-01 21:14:06 -0300

    Add files via upload

---
+++ atividade-16-a-35/atividade-19.py
@@ -0,0 +1,9 @@
+a = int(input("Digite o 1º valor: "))
+b = int(input("Digite o 2º valor: "))
+c = int(input("Digite o 3º valor: "))
+
+maior = max(a, b, c)
+menor = min(a, b, c)
+
+print(f'''Maior: {maior}
+Menor: {menor}''')
